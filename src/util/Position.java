6e09e1ab3ccd0dc120a2df2255877e6c8b12fdb9
JonoChadwell <Jono.chadwell@gmail.com>
2015-02-16 05:27:35 -0800

    Updated position

--- src/util/Position.java
+++ src/util/Position.java
@@ -3,6 +3,13 @@ package util;
 public class Position {
     public double x;
     public double y;
-    //radians, 0 east, + clockwise
-    public double rotation;
+    
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public static double distance(Position a, Position b) {
+        return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    }
 }
